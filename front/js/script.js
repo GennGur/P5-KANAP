9e5618cd446cd868a6c5f3a291419c5cbdaa0721
Gourson Glenn <114163295+GennGur@users.noreply.github.com>
2022-12-21 18:17:40 +0100

    annontation du ficher script.js

--- front/js/script.js
+++ front/js/script.js
@@ -12,36 +12,42 @@ function createCardsProduct() {
     
   //Récupèrations des données API via une "Promise" au format JSON et retourne des valeurs JS
   fetch('http://localhost:3000/api/products')
-    .then((res, ok) => {
+    .then((res) => {  
       return res.json();
     })
 
+
+    //Traitement des valeurs récuperer
     .then((products) => {
       products.forEach((product) => {
+
+        //Création des élements HTMl graçe a  l'appel de notre fonction "creatTag" (a,article,img,h3,p)
         const productLink = createTag("a");
-        const articleTag = createTag("article");
-        const productImage = createTag("img");
+        const productArticle = createTag("article");
+        const productImg = createTag("img");
         const productName = createTag("h3");
         const productDescription = createTag("p");
-        productLink.appendChild(articleTag);
-        articleTag.append(productImage, productName, productDescription);
+
+        //lien parents-enfants des élements crée chaque balise "a" contient comme élements "img","h3"et "p" 
+        //le "a" est lui définie comme enfant de la balise "items"
+        productLink.appendChild(productArticle);
+        productArticle.append(productImg, productName, productDescription);
         items.appendChild(productLink);
         
+        //définies les valeurs pour chaque élements
         productLink.href = `./product.html?id=${product._id}`;
-        
-        productImage.src = `${product.imageUrl}`;
-        productImage.alt = `${product.altTxt}`;
-
+        productImg.src = `${product.imageUrl}`;
+        productImg.alt = `${product.altTxt}`;
         productName.classList.add("productName");
         productName.textContent = `${product.name}`;
-
         productDescription.classList.add("productDescription");
         productDescription.textContent = `${product.description}`;
       });
     })
 
-    .catch((err) => {
-      console.log(`error: ${err}`);
+    // Message d'erreur dans la console si une erreur apparait 
+    .catch((error) => {
+      console.log("Erreur " + error);
     });
 }
 

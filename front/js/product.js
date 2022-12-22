4a674c8759184cc0950b82b80ae4775aaaa49cc8
Gourson Glenn <114163295+GennGur@users.noreply.github.com>
2022-12-22 16:48:56 +0100

    add product to product page

--- front/js/product.js
+++ front/js/product.js
@@ -8,3 +8,39 @@ const productId = url.searchParams.get("id");
 function createTag(TagName) {
     return document.createElement(TagName);
 };
+
+const fillProductPages = () => {
+    const apiUrl ="http://localhost:3000/api/products";
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((productList) => {
+        
+        // Trouver le produit actuel dans la liste de produits
+        const currentProduct = productList.find((product) => product._id === productId);
+       
+        //Remplir la page avec les informations du produits
+        const productImg = createTag('img');
+        document.getElementsByClassName('item__img')[0].appendChild(productImg);
+        document.getElementById('title').textContent = currentProduct.name;
+        document.getElementById('price').textContent = currentProduct.price;
+        document.getElementById('description').textContent = currentProduct.description;
+        productImg.src = currentProduct.imageUrl;
+        productImg.alt = currentProduct.altTxt;
+
+        //Options de couleur en liste déroulante
+        currentProduct.colors.forEach((colorOption) => {
+            const productColor = createTag('option');
+            document.getElementById('colors').appendChild(productColor);
+            productColor.value = colorOption;
+            productColor.textContent = colorOption;
+        });
+      })
+
+      // Si erreur message dans la console
+      .catch((err) => {
+        console.log(err);
+      });
+};
+
+fillProductPages();

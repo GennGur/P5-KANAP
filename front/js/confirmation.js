432c2f877c067b35b8a9507c0f697f0e436dc5e1
Gourson Glenn <114163295+GennGur@users.noreply.github.com>
2022-12-29 14:48:10 +0100

    confirmation function for fix order id

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,7 +1,24 @@
-const currentPageUrl = document.location.href;
-const url = new URL(currentPageUrl);
-const orderId = url.searchParams.get("order");
-const orderIdSpan = document.getElementById("orderId");
-const response = new URL("http://localhost:3000");
-console.log(response);
-orderIdSpan.textContent = `${orderId}`;
+const displayOrderConfirmation = () => {
+    // Récupère l'URL de la page courante et la stocke dans une variable
+    const currentPageUrl = document.location.href;
+  
+    // Crée une nouvelle instance de l'objet URL à partir de l'URL de la page courante
+    const url = new URL(currentPageUrl);
+  
+    // Récupère la valeur du paramètre "order" dans l'URL
+    const orderId = url.searchParams.get("order");
+  
+    // Récupère l'élément span avec l'ID "orderId"
+    const orderIdSpan = document.getElementById("orderId");
+  
+    // Crée une nouvelle instance de l'objet URL avec l'URL de l'API
+    const apiUrl = new URL("http://localhost:3000");
+  
+    // Affiche l'URL de l'API dans la console
+    console.log(apiUrl);
+  
+    // Affiche l'ID de commande dans l'élément span
+    orderIdSpan.textContent = `${orderId}`;
+  }
+  
+  displayOrderConfirmation();

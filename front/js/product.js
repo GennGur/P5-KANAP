af96b1096d6d90a2e2f8d62e3af3a93bacf01367
Gourson Glenn <114163295+GennGur@users.noreply.github.com>
2023-01-09 18:20:24 +0100

    correction méthode "fetch"

--- front/js/product.js
+++ front/js/product.js
@@ -3,6 +3,7 @@ const currentPage = window.location.href;
 //Création d'un objet URL pour ajouté l'id du produit
 const url = new URL(currentPage);
 const productId = url.searchParams.get("id");
+const apiUrl ="http://localhost:3000/api/products/";
 
 // Fonction "createTag" qui va nous permettre d'utiliser "createElement" quand on appel la fonction 
 function createTag(TagName) {
@@ -10,37 +11,32 @@ function createTag(TagName) {
 };
 
 const fillProductPages = () => {
-    const apiUrl ="http://localhost:3000/api/products";
 
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((productList) => {
-        
-        // Trouver le produit actuel dans la liste de produits
-        const currentProduct = productList.find((product) => product._id === productId);
-       
-        //Remplir la page avec les informations du produits
-        const productImg = createTag('img');
-        document.getElementsByClassName('item__img')[0].appendChild(productImg);
-        document.getElementById('title').textContent = currentProduct.name;
-        document.getElementById('price').textContent = currentProduct.price;
-        document.getElementById('description').textContent = currentProduct.description;
-        productImg.src = currentProduct.imageUrl;
-        productImg.alt = currentProduct.altTxt;
+  fetch(apiUrl + productId)
+    .then((response) => response.json())
+    .then((currentProduct) => {
+      // Remplir la page avec les informations du produit
+      const productImg = createTag('img');
+      document.getElementsByClassName('item__img')[0].appendChild(productImg);
+      document.getElementById('title').textContent = currentProduct.name;
+      document.getElementById('price').textContent = currentProduct.price;
+      document.getElementById('description').textContent = currentProduct.description;
+      productImg.src = currentProduct.imageUrl;
+      productImg.alt = currentProduct.altTxt;
 
-        //Options de couleur en liste déroulante
-        currentProduct.colors.forEach((colorOption) => {
-            const productColor = createTag('option');
-            document.getElementById('colors').appendChild(productColor);
-            productColor.value = colorOption;
-            productColor.textContent = colorOption;
-        });
-      })
-
-      // Si erreur message dans la console
-      .catch((err) => {
-        console.log(err);
+      // Options de couleur en liste déroulante
+      currentProduct.colors.forEach((colorOption) => {
+        const productColor = createTag('option');
+        document.getElementById('colors').appendChild(productColor);
+        productColor.value = colorOption;
+        productColor.textContent = colorOption;
       });
+    })
+
+    // Si erreur message dans la console
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 fillProductPages();
